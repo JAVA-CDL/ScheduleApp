5c8b4c1397a75151d13d6b5cf9db4768f632f9e9
张鑫龙 <lukelong666@163.com>
2022-01-25 09:11:00 +0800

    修改构建镜像脚本

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 echo "start........."
 
 CLOUD_USERNAME=$1
-CLOUD_PASSWORD=$2
-CLOUD_IMAGE_PREFIX=$3
-TAG=$4
+CLOUD_PASSWORD="此处填写密码"
+CLOUD_IMAGE_PREFIX=$2
+TAG=$3
 
 echo "构建镜像"
 docker build -t "$CLOUD_IMAGE_PREFIX":"$TAG" .
@@ -14,5 +14,8 @@ echo "上传镜像"
 docker login ccr.ccs.tencentyun.com --username="$CLOUD_USERNAME" --password="$CLOUD_PASSWORD"
 docker push "$CLOUD_IMAGE_PREFIX":"$TAG"
 docker push "$CLOUD_IMAGE_PREFIX":latest
+docker "删除镜像"
+docker rmi "$CLOUD_IMAGE_PREFIX":"$TAG"
+docker rmi "$CLOUD_IMAGE_PREFIX":latest
 
 echo "end........."
