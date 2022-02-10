62117df9ba6c7912bb22f9c91983207d9ed4919a
张鑫龙 <lukelong666@163.com>
2022-02-10 15:50:00 +0800

    update git ignore

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo "start........."
-
-CLOUD_USERNAME="云用户名"
-CLOUD_PASSWORD="云密码"
-CLOUD_IMAGE_PREFIX="云命名空间"
+CLOUD_USERNAME="100011397258"
+CLOUD_PASSWORD=""
+CLOUD_IMAGE_PREFIX="ccr.ccs.tencentyun.com/luke_app/hot_list"
 TAG=$1
 
 echo "构建镜像"
@@ -17,5 +15,3 @@ docker push "$CLOUD_IMAGE_PREFIX":latest
 docker "删除镜像"
 docker rmi "$CLOUD_IMAGE_PREFIX":"$TAG"
 docker rmi "$CLOUD_IMAGE_PREFIX":latest
-
-echo "end........."
