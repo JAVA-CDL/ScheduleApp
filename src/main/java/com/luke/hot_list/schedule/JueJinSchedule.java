659580415f7ec97c7245268fd96639285d715fee
张鑫龙 <lukelong666@163.com>
2022-02-22 09:36:09 +0800

    升级hutool

--- src/main/java/com/luke/hot_list/schedule/JueJinSchedule.java
+++ src/main/java/com/luke/hot_list/schedule/JueJinSchedule.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
+/**
+ * @author Administrator
+ */
 @Component
 @EnableScheduling
 public class JueJinSchedule {
