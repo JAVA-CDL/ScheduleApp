7fcf7bf56c7785091c40a66ce04696886d44d35b
张鑫龙 <lukelong666@163.com>
2022-03-04 10:18:39 +0800

    添加全局异常处理

--- src/main/java/com/luke/hot_list/entity/Handbook.java
+++ src/main/java/com/luke/hot_list/entity/Handbook.java
@@ -1,18 +1,45 @@
 package com.luke.hot_list.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * @author luke_long
  * @version 1.0
  * @date 2022/2/23 9:23
  */
-@Getter
-@Setter
 public class Handbook {
     private Integer id;
     private String name;
     private String description;
     private String imgUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
 }
---
+++ src/main/java/com/luke/hot_list/utils/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.luke.hot_list.utils;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        return "服务器内部异常";
+    }
+}
