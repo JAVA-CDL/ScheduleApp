ec162cbd05d5192641cc2dec9b5a7fe2edc6db3e
张鑫龙 <lukelong666@163.com>
2022-03-09 16:54:57 +0800

    日志记录

--- src/main/java/com/luke/hot_list/controller/CommentController.java
+++ src/main/java/com/luke/hot_list/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.luke.hot_list.controller;
 
 import cn.hutool.core.date.DateUtil;
+import com.luke.hot_list.annotation.Log;
 import com.luke.hot_list.dao.CommentDao;
 import com.luke.hot_list.entity.Comment;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +20,14 @@ public class CommentController {
     @Resource
     private CommentDao commentDao;
 
+    @Log(title = "保存评论")
     @PostMapping("/save")
     public void save(@RequestBody Comment comment) {
         comment.setCreateTime(DateUtil.now());
         commentDao.save(comment);
     }
 
+    @Log(title = "获取所有评论")
     @GetMapping("/getAll")
     public List<Comment> getAll() {
         return commentDao.getAll();
